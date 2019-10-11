60e669a0b0777dd309440510829542c3797894dd
jacky <clorisforcoding@gmail.com>
2019-10-12 01:27:52 +0800

    Initial commit

---
+++ src/test/java/cn/teamwang/elk/ElkLogSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.teamwang.elk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElkLogSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
